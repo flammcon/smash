082c6393894182336a3f1263e24ac42e4212d7d9
Marlene Cota <marlenecota@gmail.com>
2020-03-06 02:21:05 -0800

    2v2 bracket template

--- src/components/2v2.vue
+++ src/components/2v2.vue
@@ -5,52 +5,7 @@
       <TwoVsTwoSeeding/>
     </div>
     <div class="col" v-if="teams.length > 0">
-      <h3>Winner's Bracket</h3>
-      <div class="row">
-        <div class="col">
-          <div class="row">
-            <ul style="padding: 5px;" class="list-group list-group-horizontal">
-              <li class="list-group-item">{{teams[0].player1.name}}</li>
-              <li class="list-group-item">{{teams[0].player2.name}}</li>  
-            </ul>
-          </div>
-          <div class="row">            
-            <ul style="padding: 5px;" class="list-group list-group-horizontal">
-              <li class="list-group-item">{{teams[3].player1.name}}</li>
-              <li class="list-group-item">{{teams[3].player2.name}}</li>  
-            </ul>
-          </div>
-          <div class="row">
-            <ul style="padding: 5px;" class="list-group list-group-horizontal">
-              <li class="list-group-item">{{teams[2].player1.name}}</li>
-              <li class="list-group-item">{{teams[2].player2.name}}</li>  
-            </ul>
-          </div>
-          <div class="row">
-            <ul style="padding: 5px;" class="list-group list-group-horizontal">
-              <li class="list-group-item">{{teams[1].player1.name}}</li>
-              <li class="list-group-item">{{teams[1].player2.name}}</li>  
-            </ul>
-          </div>
-        </div>
-        <div class="col">
-          <div class="row">Winner Game1</div>
-          <div class="row">Winner Game2</div>
-        </div>
-        <div class="col">
-          winner
-        </div>
-      </div>
-      <h3>Loser's Bracket</h3>
-      <div class="row">
-        <div class="col">
-          <div class="row">Loser Game1</div>
-          <div class="row">Loser Game2</div>
-        </div>
-        <div class="col">
-          3rd place
-        </div>
-      </div>
+      <Bracket/>
     </div>
   </div>
 </template>
@@ -58,11 +13,13 @@
 <script>
 import { mapState } from 'vuex'
 import TwoVsTwoSeeding from './2v2v2v2';
+import Bracket from './Bracket';
 
 export default {
   name: 'TwoVsTwo',
   components: {
-    TwoVsTwoSeeding
+    TwoVsTwoSeeding,
+    Bracket
   },
   computed: {
     ...mapState({
